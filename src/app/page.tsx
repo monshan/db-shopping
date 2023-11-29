7e95c8f97e13676198f11a287fa7992789ccf016
Marika Shanahan <marikashanahan@gmail.com>
2023-11-29 12:51:31 -0700

    ShoppingListContext and custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,26 @@ import ShoppingList from '@/components/ShoppingList'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-export interface ListItems {
+export type ListItems = {
   [key: string]: {
     quantity: number,
     isComplete: boolean
   }
 }
 
+// can we move any of these states down to the components?
+
 export default function Home() {
   const [query, setQuery] = useState('')
   const [listItems, setListItems] = useState<ListItems>({})
   const { data, error, isLoading } = useSWR(() => { return query.length > 1 && `https://api.frontendeval.com/fake/food/${query}`}, fetcher, { dedupingInterval: 500 })
 
-  const inputHandler = (e) => setQuery(e.target.value)
+  // const inputHandler = (e) => setQuery(e.target.value)
     
   return (
-    <main className="flex flex-col items-center pt-24 min-h-screen">
+    <main className="flex flex-col items-center pt-20 min-h-screen">
       <h1 className='text-5xl'>My Shopping List</h1>
-      <input type="text" className="border-2 border-solid border-stone-800 p-2 mt-8" onChange={inputHandler} />
+      <input type="text" className="border-2 border-solid border-stone-800 p-2 mt-8" onChange={(e) => setQuery(e.target.value)} />
       <DynamicList queryData={data} setListItems={setListItems} listItems={listItems} isLoading={isLoading}/>
       <ShoppingList listItems={listItems} setListItems={setListItems} />
     </main>
---
+++ src/components/ShoppingListContext.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import React, { useContext, createContext, useState } from "react"
+import { ListItems } from "@/app/page"
+
+type ShoppingListContextType = {
+    listItems: ListItems,
+    setListItems: React.Dispatch<React.SetStateAction<ListItems>>
+}
+
+type ShoppingListContextProps = {
+    children: React.ReactNode
+}
+
+export const ShoppingListTheme = createContext<ShoppingListContextType | null>(null)
+
+export default function ShoppingListContext({ children }: ShoppingListContextProps) {
+    const [listItems, setListItems] = useState<ListItems>({})
+    
+    return (
+        <ShoppingListTheme.Provider value={{ listItems, setListItems }}>
+            {children}
+        </ShoppingListTheme.Provider>
+    )
+}
+
+export function useShoppingList() {
+    const context = useContext(ShoppingListTheme)
+    if (context === undefined) {
+        throw new Error("useShoppingList must be used within a ShoppingListContext")
+    }
+
+    return context
+}
